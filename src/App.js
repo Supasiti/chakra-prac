2a1485ca597b99d115219c1927e2c9007f738dea
Thara Supasiti <sapiensis@gmail.com>
2021-10-11 16:54:57 +1100

    add form'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Box, Flex, Container, Text, VStack,Input, InputGroup, InputLeftElement, FormLabel, FormControl,  Button } from "@chakra-ui/react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {  LockIcon } from '@chakra-ui/icons'
+
+const App = () => (
+  <ChakraProvider>
+    <Container maxW="24rem" p="0" minH="100vh">
+      <Flex minH="100vh" flexDirection="column" justifyContent="center">
+        <Box w="100%"  m="auto" p="4" border="1px" borderColor="gray.200" rounded='2xl'>
+          <form >
+            <VStack spacing={4}>
+              <Text as="h2" fontSize="3xl" textAlign="center">
+                Login
+              </Text>
+              
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children="@"/>
+                  <Input type="email" placeholder="bob@youruncle.com" />
+                </InputGroup>
+              </FormControl>
+
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<LockIcon  w={4} h={4}/>}/>
+                  <Input type="password" placeholder="password" />
+                </InputGroup>
+              </FormControl>
+
+              <Button w="100%" colorScheme="teal" size="md">
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </Flex>
+    </Container>
+  </ChakraProvider>
+  
+);
 
 export default App;
